c940470999e2d7be09fc1b0fd780ccc3bdb2dc98
MrABKhan <theawesomekhan@gmail.com>
2021-04-10 22:32:28 +0300

    refactoring

---
+++ src/chapter10/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+func callMyName(){
+	fmt.Println("My Name is Khan")
+	return
+}
+
+func main() {
+	callMyName()
+}
--- src/chapter9/main.go
+++ src/chapter9/main.go
@@ -16,25 +16,46 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 	roomsOccupied := rand.Intn(134)
 	roomsAvailable := numberOfRooms - roomsOccupied
-	var occupancy float64 = (float64(roomsOccupied) / float64(numberOfRooms)) * 100
-	if occupancy >= 60 {
-		fmt.Println("Occupancy Rate : ", "High")
-	} else if occupancy >= 30 {
-		fmt.Println("Occupancy Rate : ", "Medium")
-	} else {
-		fmt.Println("Occupancy Rate : ", "Low")
-	}
-	fmt.Println("Occupancy Percentage : ", occupancy)
+
+	printOccupancyLevel(computeOccupancyLevel(roomsOccupied, numberOfRooms))
 
 	if roomsAvailable > 0 {
 		for i := 0; i < roomsAvailable; i++ {
-			roomNumber := startingRoomNumber + i
-			people := rand.Intn(10-1) + 1
-			nights := rand.Intn(6-1) + 1
-			fmt.Println(roomNumber, " : ", people, " people ", nights, " nights")
+			printCurrentRoomStatus(startingRoomNumber, i)
 		}
 	} else {
 		fmt.Println("No rooms are available tonight")
 	}
 
+}
+
+func printCurrentRoomStatus(startingRoomNumber int, currentRoomNumber int) {
+	roomNumber := startingRoomNumber + currentRoomNumber
+	people := rand.Intn(10-1) + 1
+	nights := rand.Intn(6-1) + 1
+	nightText := " night"
+	peopleText := " person "
+	if nights > 1 {
+		nightText = " nights"
+	}
+	if people > 1 {
+		peopleText = " people "
+	}
+	fmt.Println(roomNumber, " : ", people, peopleText, nights, nightText)
+}
+
+func computeOccupancyLevel(roomsOccupied int, numberOfRooms int) float64 {
+	return (float64(roomsOccupied) / float64(numberOfRooms)) * 100
+}
+
+func printOccupancyLevel(occupancy float64) {
+	switch {
+	case occupancy >= 60:
+		fmt.Println("Occupancy Rate : ", "High")
+	case occupancy >= 30:
+		fmt.Println("Occupancy Rate : ", "Medium")
+	default:
+		fmt.Println("Occupancy Rate : ", "Low")
+	}
+	fmt.Printf("Occupancy Percentage : %0.2f %% \n", occupancy)
 }
