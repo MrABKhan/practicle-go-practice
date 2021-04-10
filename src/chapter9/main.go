3538b3fc8a841e5c1a2bde23a3f0745111f2bfb2
bekircci <77629475+bekircci@users.noreply.github.com>
2021-04-10 19:59:03 +0300

    -- added main go file -- initial commit

---
+++ src/chapter9/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+func main() {
+	// Initializing Constants
+	const hotelName string = "Gopher Paris Inn"
+	const numberOfRooms uint8 = 134
+	// Seeding Time
+	rand.Seed(time.Now().UnixNano())
+
+}
